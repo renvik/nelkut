5bda59a9606aad9c4dfd8dc07585318e73900f71
Thomas Grundström <thomas.grundstrom@helsinki.fi>
2023-11-21 13:06:00 +0200

    edit refservice.py

--- refservice.py
+++ refservice.py
@@ -1,7 +1,16 @@
 from db import db
+from sqlalchemy.sql import text
 
 def add_book_to_database(request):
-    
+    title = request["title"]
+    author = request["author"]
+    year = request["year"]
+    publisher = request["publisher"]
+    start_page = request["start_page"]
+    end_page = request["end_page"]
+    cite_id = request["cite_id"]
+    sql = text("INSERT INTO books (cite_id, title, author, year, publisher, start_page, end_page) VALUES (:cite_id, :title, :author, :year, :publisher, :start_page, :end_page)")
+    db.session.execute(sql, {"books"})
         
 
 #    def validate_reference(self, reference):
