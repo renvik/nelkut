ef4e54db7d48c1b9a4e6fdfcb7161aaf9459bd0e
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-11-21 11:18:56 +0200

    start function to get references

--- db.py
+++ db.py
@@ -1,5 +1,14 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///nelkut"
 db = SQLAlchemy(app)
+
+def list_references():
+    sql = "SELECT * FROM book"
+    books = db.session.execute(text(sql))
+    sql = "SELECT * FROM article"
+    articles = db.session.execute(text(sql))
+    sql = "SELECT * FROM inproceedings"
+    inproceedings = db.session.execute(text(sql))
