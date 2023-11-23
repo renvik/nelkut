c95091355909c7db3e474d55705ee97cd9a02657
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-11-23 14:10:29 +0200

    fix initial reference listing

--- db.py
+++ db.py
@@ -6,10 +6,17 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///nelkut"
 db = SQLAlchemy(app)
 
 def list_references():
+    references = []
     sql = "SELECT * FROM book"
     books = db.session.execute(text(sql)).fetchall()
+    for i in books:
+        references.append(i)
     sql = "SELECT * FROM article"
     articles = db.session.execute(text(sql)).fetchall()
+    for i in articles:
+        references.append(i)
     sql = "SELECT * FROM inproceedings"
     inproceedings = db.session.execute(text(sql)).fetchall()
-    return books, articles, inproceedings
+    for i in inproceedings:
+        references.append(i)
+    return references
