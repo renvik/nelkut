94e6d251a4ec2ada7a10b203518ed07e5f30b10f
Thomas Grundström <thomas.grundstrom@helsinki.fi>
2023-11-28 13:37:56 +0200

    add test_schema file

---
+++ tests/test_schema.sql
@@ -0,0 +1,47 @@
+DROP DATABASE IF EXISTS test_nelkut;
+
+CREATE DATABASE test_nelkut;
+\c test_nelkut;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT
+);
+
+CREATE TABLE inproceedings (
+    id SERIAL PRIMARY KEY,
+    cite_id TEXT UNIQUE,
+    author TEXT,
+    title TEXT,
+    year INTEGER,
+    booktitle TEXT,
+    start_page INTEGER,
+    end_page INTEGER,
+    user_id INTEGER REFERENCES users
+);
+
+CREATE TABLE articles (
+    id SERIAL PRIMARY KEY,
+    cite_id TEXT UNIQUE,
+    author TEXT,
+    title TEXT,
+    journal TEXT,
+    year INTEGER,
+    volume INTEGER,
+    start_page INTEGER,
+    end_page INTEGER,
+    user_id INTEGER REFERENCES users
+);
+
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    cite_id TEXT UNIQUE,
+    author TEXT,
+    title TEXT,
+    year INTEGER,
+    publisher TEXT,
+    start_page INTEGER,
+    end_page INTEGER,
+    user_id INTEGER REFERENCES users
+);
