ad1095bc64ae894e450e65679dc8a60c5ee8cc8b
pants64DS <pants64DS@users.noreply.github.com>
2023-11-28 14:01:42 +0200

    Convert indentation to tabs

--- reference_list.py
+++ reference_list.py
@@ -3,19 +3,19 @@ from sqlalchemy.sql import text
 
 
 def list_books(db):
-    sql = "SELECT * FROM books"
-    books = db.session.execute(text(sql)).fetchall()
-    return books
+	sql = "SELECT * FROM books"
+	books = db.session.execute(text(sql)).fetchall()
+	return books
 
 def list_articles(db):
-    sql = "SELECT * FROM articles"
-    articles = db.session.execute(text(sql)).fetchall()
-    return articles
+	sql = "SELECT * FROM articles"
+	articles = db.session.execute(text(sql)).fetchall()
+	return articles
 
 def list_inproceedings(db):
-    sql = "SELECT * FROM inproceedings"
-    inproceedings = db.session.execute(text(sql)).fetchall()
-    return inproceedings
+	sql = "SELECT * FROM inproceedings"
+	inproceedings = db.session.execute(text(sql)).fetchall()
+	return inproceedings
 
 def list_references(db):
 	books = list_books(db)
