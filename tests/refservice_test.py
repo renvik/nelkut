d327f9dae56aa523c9841f8ee79bc9db840e4004
Thomas Grundström <thomas.grundstrom@helsinki.fi>
2023-11-30 16:42:06 +0200

    update refservice test file

--- tests/refservice_test.py
+++ tests/refservice_test.py
@@ -34,11 +34,42 @@ class RefServiceTest(unittest.TestCase):
             books = self.db.session.execute(text("SELECT * FROM books")).fetchall()
             self.assertEqual(len(books), 3)
 
+    def test_add_article_to_database(self):
+        
+        with self.app.app_context():
+            self.db.init_app(self.app)
+            request = MockupRequest({"cite_id": "smfokmsc", 
+                                    "author": "jfpapoksad", 
+                                    "title": "adslkjasldk", 
+                                    "journal": "lafdsjajds", 
+                                    "year": 1, 
+                                    "volume": 2, 
+                                    "start_page": 3, 
+                                    "end_page": 4})
+            refservice.add_article_to_database(self.db, request)
+            articles = self.db.session.execute(text("SELECT * FROM articles")).fetchall()
+            self.assertEqual(len(articles), 3)
+
+    def test_add_inproceeding_to_database(self):
+        
+        with self.app.app_context():
+            self.db.init_app(self.app)
+            request = MockupRequest({"cite_id": "ldjfajsja", 
+                                    "author": "lasjdljsadk", 
+                                    "title": "lajdsjaslkdjö", 
+                                    "year": 1, 
+                                    "booktitle": "sapjdajd", 
+                                    "start_page": 2, 
+                                    "end_page": 3})
+            refservice.add_inproceeding_to_database(self.db, request)
+            inproceedings = self.db.session.execute(text("SELECT * FROM inproceedings")).fetchall()
+            self.assertEqual(len(inproceedings), 3)
+
     def test_lists_length(self):
 
         with self.app.app_context():
             self.db.init_app(self.app)
             books, articles, inproceedings = refservice.list_references(self.db)
             self.assertEqual(len(books), 3)
-            self.assertEqual(len(articles), 2)
-            self.assertEqual(len(inproceedings), 2)
+            self.assertEqual(len(articles), 3)
+            self.assertEqual(len(inproceedings), 3)
