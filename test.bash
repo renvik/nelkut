3f2c4d9c89a8a15d04aa55a63cbe5e59835b7e94
Thomas Grundström <thomas.grundstrom@helsinki.fi>
2023-11-30 16:23:21 +0200

    modify test.bash file

--- test.bash
+++ test.bash
@@ -1,5 +1,5 @@
-DB_URL=postgresql://postgres:savusauna@localhost:5432/nelkut_tests
+DB_URL=postgresql:///nelkut_tests
 
-psql "$DB_URL" < "$(dirname $0)\test_schema.sql"
+psql "$DB_URL" < test_schema.sql
 
 poetry run pytest
