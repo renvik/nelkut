ff44029d8b2c15892d265ef45f6ed0422d5534bd
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-12-01 11:50:46 +0200

    refine add-tests

--- tests/refservice_test.py
+++ tests/refservice_test.py
@@ -19,6 +19,7 @@ class RefServiceTest(unittest.TestCase):
         
         with self.app.app_context():
             self.db.init_app(self.app)
+            initial_amount = len(refservice.list_books(self.db))
             request = MockupRequest({"cite_id": "abcdefghijk", 
                                      "author": "Some Author", 
                                      "title": "Some Title", 
@@ -27,13 +28,14 @@ class RefServiceTest(unittest.TestCase):
                                      "start_page": 1, 
                                      "end_page": 2})
             refservice.add_book_to_database(self.db, request)
-            books = self.db.session.execute(text("SELECT * FROM books")).fetchall()
-            self.assertEqual(len(books), 3)
+            books = refservice.list_books(self.db)
+            self.assertEqual(len(books), initial_amount + 1)
 
     def test_add_article_to_database(self):
         
         with self.app.app_context():
             self.db.init_app(self.app)
+            initial_amount = len(refservice.list_articles(self.db))
             request = MockupRequest({"cite_id": "smfokmsc", 
                                     "author": "jfpapoksad", 
                                     "title": "adslkjasldk", 
@@ -43,13 +45,14 @@ class RefServiceTest(unittest.TestCase):
                                     "start_page": 3, 
                                     "end_page": 4})
             refservice.add_article_to_database(self.db, request)
-            articles = self.db.session.execute(text("SELECT * FROM articles")).fetchall()
-            self.assertEqual(len(articles), 3)
+            articles = refservice.list_articles(self.db)
+            self.assertEqual(len(articles), initial_amount + 1)
 
     def test_add_inproceeding_to_database(self):
         
         with self.app.app_context():
             self.db.init_app(self.app)
+            initial_amount = len(refservice.list_inproceedings(self.db))
             request = MockupRequest({"cite_id": "ldjfajsja", 
                                     "author": "lasjdljsadk", 
                                     "title": "lajdsjaslkdjö", 
@@ -58,8 +61,8 @@ class RefServiceTest(unittest.TestCase):
                                     "start_page": 2, 
                                     "end_page": 3})
             refservice.add_inproceeding_to_database(self.db, request)
-            inproceedings = self.db.session.execute(text("SELECT * FROM inproceedings")).fetchall()
-            self.assertEqual(len(inproceedings), 3)
+            inproceedings = refservice.list_inproceedings(self.db)
+            self.assertEqual(len(inproceedings), initial_amount + 1)
 
     def test_lists_length(self):
 
