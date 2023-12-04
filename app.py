e0b7be5ccc78591ab5c0567ae61b997458d53055
Vesa Rautiainen <veskurau@gmail.com>
2023-12-04 15:17:40 +0200

    Add functionality for login and registration

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE")
+app.secret_key = getenv("SECRET_KEY")
 db.init_app(app)
 
 import routes
--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from flask import request, redirect, render_template, send_from_directory, session
 from app import app
 import refservice
+import users
 from db import db
 from bibtex_creator import write_bibtex_file, sort_entries
 import os
@@ -53,3 +54,35 @@ def bibtex():
 @app.route("/download")
 def download():
 	return send_from_directory(app.root_path, "bibtex.bib", as_attachment=True)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Wrong username or password")
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="Passwords do not match")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Registration failed")
