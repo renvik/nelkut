2bc0e22505f4c5046712a694796345630ba60bba
Sonja Olkkonen <sonja.olkkonen@gmail.com>
2023-12-04 10:01:05 +0200

    Modify the refservice functions so that user id is saved properly to references

--- refservice.py
+++ refservice.py
@@ -7,19 +7,30 @@ def __insert(db, table_name, keys, request):
 	db.session.commit()
 
 def add_inproceeding_to_database(db, request):
-	keys = ["cite_id", "author", "title", "year", "booktitle", "start_page", "end_page"]
+	keys = ["cite_id", "author", "title", "year", "booktitle", "start_page", "end_page", "user_id"]
+	check_users_cite_id_duplicate(request.form["cite_id"], request.form["user_id"], db)
 
 	__insert(db, "inproceedings", keys, request)
 
 def add_article_to_database(db, request):
-	keys = ["cite_id", "author", "title", "journal", "year", "volume", "start_page", "end_page"]
+	keys = ["cite_id", "author", "title", "journal", "year", "volume", "start_page", "end_page", "user_id"]
+	check_users_cite_id_duplicate(request.form["cite_id"], request.form["user_id"], db)
 
 	__insert(db, "articles", keys, request)
 
 def add_book_to_database(db, request):
-	keys = ["cite_id", "author", "title", "year", "publisher", "start_page", "end_page"]
+	keys = ["cite_id", "author", "title", "year", "publisher", "start_page", "end_page", "user_id"]
+	check_users_cite_id_duplicate(request.form["cite_id"], request.form["user_id"], db)
 
 	__insert(db, "books", keys, request)
+
+def check_users_cite_id_duplicate(cite_id, user_id, db):
+	for i in ["books", "articles", "inproceedings"]:
+		sql = f"SELECT * FROM {i} WHERE cite_id=:cite_id AND user_id=:user_id"
+		check = db.session.execute(text(sql), {"cite_id":cite_id,"user_id":user_id}).fetchall()
+		if not check:
+			return False
+	return True
 
 def list_books(db):
 	sql = "SELECT * FROM books"
